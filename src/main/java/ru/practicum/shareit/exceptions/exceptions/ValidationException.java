edcef89498b72c31b363e295e7b3b8200548220a
AVG <glazyrinav1986@yandex.ru>
2023-06-19 15:29:49 +0300

    - первая стабильная версия

--- src/main/java/ru/practicum/shareit/exceptions/exceptions/ValidationException.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.shareit.exceptions.exceptions;
-
-public class ValidationException extends javax.validation.ValidationException {
-
-    public ValidationException(String msg) {
-        super(msg);
-    }
-
-}
